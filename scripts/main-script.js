00dd612ffd2ef1ffff7826a265df4a8779f88280
Christopher <joat79@braxmail.net>
2025-10-22 11:39:37 -0500

    Added array for new posts and removed old posts

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -7,3 +7,25 @@ if (dateEl) {
     day: "numeric"
   });
 }
+
+/*
+
+let imageIndex = 0;
+const fallbackImages = [
+  "../imagesbg1.jpg",
+  "../imagesbg2.jpg",
+  "../imagesbg3.jpg",
+  "../imagesbg4.jpg",
+  // ...
+];
+
+function getImage(post) {
+  if(post.img) return post.img;
+  const img = fallbackImage[imageIndex % fallbackImages.length];
+  imageIndex++;
+  return img;
+}
+
+<img src="${getImage(post)}" alt="${post.caption || 'Blog image'}">
+
+*/
---
+++ scripts/posts.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/*
+
+Template
+
+  {
+    id: "",
+    date: "",
+    title: "",
+    content: "",
+    img: "",
+    caption: "",
+  },
+
+*/
+
+const posts = [
+  {
+    id: "massive-overhaul",
+    date: "2025-08-18T10:53",
+    title: "Massive Overhaul",
+    content: "This entire site is getting redone...",
+    img: "../images/404lightpage.jpg",
+    caption: "I've been lost for a bit.",
+  },
+];
