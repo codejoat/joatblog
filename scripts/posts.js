761b21d49176db36200aa0621d478f98a14ecc90
Christopher <joat79@braxmail.net>
2025-10-25 11:31:39 -0500

    Changed and updated archiive format

--- scripts/posts.js
+++ scripts/posts.js
@@ -17,11 +17,19 @@ Template
 
 const posts = [
   {
+    id: "some-ideas",
+    date: "2025-10-25T10:13",
+    title: "Some Ideas",
+    content: "I have some ideas on what I can do to improve my performance as well as the aesthetics of the page. One is to always include a picture, but I would just do a revolving doxen or so pictures, and if I had a relevant picture at the time, I would build a way to insert it into the queue. Also on this end, I am moving the posts from the HTML to the JavaScript and creating a function for it to put the posts where they belong. No more copy-pasta. Also, why did I set line-wrap to false. This is annoying.",
+    img: "../images/404page.jpg",
+    caption: "I'm getting back on track.",
+  },
+  {
     id: "massive-overhaul",
-    date: "2025-08-18T10:53",
+    date: "2025-10-18T10:53",
     title: "Massive Overhaul",
     content: "This entire site is getting redone...",
     img: "../images/404lightpage.jpg",
-    caption: "I've been lost for a bit.",
-  },
+    caption: "I've been lost for a bit."
+  }
 ];
